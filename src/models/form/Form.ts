da11be510ff0bd2f69479d84bb24a37998e04bbf
Danilo Leão <olin_ad@hotmail.com>
2023-03-24 09:35:30 -0300

    feat: getter method for Form attrs

--- src/models/form/Form.ts
+++ src/models/form/Form.ts
@@ -4,4 +4,8 @@ export class Form {
   constructor(
     private attributes: FormAttributes
   ) {}
+
+  get<K extends keyof  FormAttributes>(attribute: K): FormAttributes[K]  {
+    return this.attributes[attribute];
+  }
 };
